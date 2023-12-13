734479b4e952a6169650530cc50ccef53a6fadde
m5623skhj <m5623skhj@gmail.com>
2023-12-13 23:17:17 +0900

    * IOCPTimer 구현 ** 아직 테스트를 해보지는 않아서, 정상 작동할지는 잘 모르겠음 ** 각 타이머의 등록과 정지의 책임은 타이머를 생성하는 코드에서 갖기로 함

--- DdingGameServer/RIO_Core/IOCPTimer.h
+++ DdingGameServer/RIO_Core/IOCPTimer.h
@@ -1,33 +1,63 @@
 #pragma once
 #include <thread>
+#include <mutex>
+#include <map>
 
 class IOCPTimer
 {
 public:
-	IOCPTimer() = default;
+	IOCPTimer();
 	virtual ~IOCPTimer() = default;
 
 public:
 	virtual void StartTimer() = 0;
 	virtual void OnTimer() = 0;
-	virtual void ResumeTimer() = 0;
 
 protected:
-	void SetTick(UINT64 tick) { timerTick = tick; }
+	void ResumeTimer();
+	
+	void SetNextWakeUpTick(UINT64 nextWakeUpTime) { wakeUpTime = nextWakeUpTime; }
+
+public:
+	UINT64 GetTimerId() { return timerId; }
 
 private:
-	UINT64 timerTick = 0;
+	UINT64 timerId = 0;
+	UINT64 wakeUpTime = 0;
+	OVERLAPPED overlapped;
 };
 
 class IOCPTimerManager
 {
-public:
+private:
 	IOCPTimerManager();
 	~IOCPTimerManager();
 
 public:
+	static IOCPTimerManager& GetInst()
+	{
+		static IOCPTimerManager instance;
+		return instance;
+	}
+
+public:
+	UINT64 GetTimerId() { return nextTimerId++; }
 	void RunTimerThread();
 
+public:
+	void RegisterTimer(std::shared_ptr<IOCPTimer> timer);
+	void ResumeTimer(UINT64 timerId, LPOVERLAPPED timerOverlapped);
+	void StopTimer(UINT64 timerId);
+	void StopTimerManager();
+
 private:
+	HANDLE iocpHandle;
 	std::thread timerThread;
+	std::mutex timerMapLock;
+	std::map<UINT64, std::shared_ptr<IOCPTimer>> timerMap;
+
+	bool isValid = true;
+	bool isRunning = false;
+
+	std::atomic<UINT64> nextTimerId = 1;
 };
