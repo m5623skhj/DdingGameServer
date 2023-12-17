28026cf37c6bf2cecb087a93d7b7f231715df5c1
m5623skhj <m5623skhj@gmail.com>
2023-12-18 00:28:54 +0900

    * ScriptToObject 폴더 구조 변경 ** 이하 폴더들을 전부 한 계층씩 위로 올림 * 기본 프로젝트 경로 및 바이너리의 옵션 파일들을 각 폴더로 관리하도록 변경 ** 규칙 추가 : 모든 바이너리 출력 폴더로의 옵션 파일은 각자의 이름+OptionFile\ 로 해당 폴더를 명명한다 ** DBServer : DBServerOptionFile\ ** RIO_Core : RIO_CoreOptionFile\ ** ScriptToObject : ScriptToObjectOptionFile\

--- ScriptToObject/ScriptToObject/main.cpp
+++ ScriptToObject/ScriptToObject/main.cpp
@@ -1,5 +1,4 @@
 ﻿#include <iostream>
-#include "TestData.h"
 #include "DataUtil.h"
 #include "JsonUtil.h"
 
@@ -13,11 +12,6 @@ int main()
 		cout << "Failed to load data" << std::endl;
 		return 0;
 	}
-
-	auto temp1 = FIND_DATA(Test1, 2);
-	auto tempErr = FIND_DATA(Test1, 100);
-
-	auto temp2 = FIND_DATA(Test2, "test_2");
 
 	return 0;
 }
