3dd3675df1f95db3965da679d10c2a0e764f7121
m5623skhj <m5623skhj@gmail.com>
2023-12-19 00:27:14 +0900

    * PCManager 인터페이스만 미리 생성 ** SessionId나 PCId 둘 중 하나로만 Add, Delete 해도 두 개의 맵 모두에 한꺼번에 반영됐으면 좋겠음

--- GameServer/GameServer/PCManager.cpp
+++ GameServer/GameServer/PCManager.cpp
@@ -6,3 +6,47 @@ PCManager& PCManager::GetInst()
 	static PCManager instance;
 	return instance;
 }
+
+void PCManager::AddPCBySessionId(SessionId sessionId, std::shared_ptr<PC> pc)
+{
+
+}
+
+void PCManager::AddPCByPCId(PCId pcId, std::shared_ptr<PC> pc)
+{
+
+}
+
+void PCManager::DeletePCBySessionId(SessionId sessionId)
+{
+
+}
+
+void PCManager::DeletePCByPCId(PCId pcId)
+{
+
+}
+
+std::shared_ptr<PC> PCManager::FindPCBySessionId(SessionId sessionId)
+{
+	std::lock_guard<std::mutex> lock(sessionIdToPCMapLock);
+	auto findIter = sessionIdToPCMap.find(sessionId);
+	if (findIter == sessionIdToPCMap.end())
+	{
+		return nullptr;
+	}
+
+	return findIter->second;
+}
+
+std::shared_ptr<PC> PCManager::FindPCByPCId(PCId pcId)
+{
+	std::lock_guard<std::mutex> lock(pcIdToPCMapLock);
+	auto findIter = pcIdToPCMap.find(pcId);
+	if (findIter == pcIdToPCMap.end())
+	{
+		return nullptr;
+	}
+
+	return findIter->second;
+}
--- GameServer/GameServer/PCManager.h
+++ GameServer/GameServer/PCManager.h
@@ -9,8 +9,24 @@ class PC;
 
 class PCManager
 {
+private:
+	PCManager() = default;
+	~PCManager() = default;
+
 public :
 	PCManager& GetInst();
+
+public:
+	// TODO :
+	// SessionId나 PCId 둘 중 하나로만 Add, Delete 해도 두 개 모두에 들어갔으면 좋겠음
+	void AddPCBySessionId(SessionId sessionId, std::shared_ptr<PC> pc);
+	void AddPCByPCId(PCId pcId, std::shared_ptr<PC> pc);
+
+	void DeletePCBySessionId(SessionId sessionId);
+	void DeletePCByPCId(PCId pcId);
+
+	std::shared_ptr<PC> FindPCBySessionId(SessionId sessionId);
+	std::shared_ptr<PC> FindPCByPCId(PCId pcId);
 
 private:
 	std::unordered_map<SessionId, std::shared_ptr<PC>> sessionIdToPCMap;
