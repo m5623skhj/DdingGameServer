b76f4ac122c411f72ef4fc8631d099e6353b0679
m5623skhj <m5623skhj@gmail.com>
2023-12-25 03:09:58 +0900

    * main문 작성 ** 빌드 제외 되어있던 main.cpp 파일 빌드에 포함하도록 수정

--- GameServer/GameServer/main.cpp
+++ GameServer/GameServer/main.cpp
@@ -1,6 +1,32 @@
 #include "PreCompile.h"
+#include "RIOServer.h"
+#include "PacketManager.h"
+#include "DeadlockChecker.h"
+#include "DBClient.h"
 
 int main()
 {
+	{
+		PacketManager::GetInst().Init();
+	}
+	//DBClient::GetInstance().Start(L"OptionFile/DBClientOptionFile.txt");
+
+	RIOServer& server = RIOServer::GetInst();
+	server.StartServer(L"OptionFile/ServerOption.txt");
+
+	while (true)
+	{
+		Sleep(1000);
+
+		std::cout << server.GetSessionCount() << std::endl;
+
+		if (GetAsyncKeyState(VK_ESCAPE) & 0x8000)
+		{
+			server.StopServer();
+			break;
+		}
+	}
+	server.StopServer();
+
 	return 0;
 }
