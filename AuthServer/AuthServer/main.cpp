0e027d4458f3ec0321b4926d8d0cdf1eabf0fd3e
m5623skhj <m5623skhj@gmail.com>
2024-01-21 19:00:12 +0900

    * AuthServer 실행 가능하도록 수정 * AuthServer 빌드 이벤트 추가

--- AuthServer/AuthServer/main.cpp
+++ AuthServer/AuthServer/main.cpp
@@ -4,7 +4,19 @@
 
 int main()
 {
+	const std::wstring folderPath = L"AuthServerOptionFile/";
 
+	if (AuthServer::GetInst().StartAuthServer(folderPath + L"LanServerOptionFile.txt", folderPath + L"NetServerOptionFile.txt") == false)
+	{
+		std::cout << "Auth server start failed" << std::endl;
+		return 0;
+	}
+	std::cout << "Auth server running" << std::endl;
+
+	while (1)
+	{
+		// do what?
+	}
 
 	return 0;
 }
