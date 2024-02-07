4fa1916d99bbdf4f801f68305f50b86124f4c1c9
m5623skhj <m5623skhj@gmail.com>
2024-02-07 18:45:58 +0900

    * AuthServer 쿼리문들의 결과를 필요로 하는지, 혹은 필요로 하지 않는지를 표현하기 위해 QueryObject로 쿼리를 래핑 ** DB 처리 이후, 결과가 필요한지에 대한 분기가 필요하기 때문에 수정함 ** AuthQuerySet의 생성자에서 쿼리들을 등록하고 관리하도록 함

---
+++ AuthServer/AuthServer/AuthQuery.cpp
@@ -0,0 +1,9 @@
+#include "PreCompile.h"
+#include "AuthQuery.h"
+
+AuthQuerySet::AuthQuerySet() 
+	// :
+	// Register queries
+	// exampleQueryObject(L"{CALL ExampleSPName(?, ?)}", false)
+{
+}
--- AuthServer/AuthServer/AuthQuery.h
+++ AuthServer/AuthServer/AuthQuery.h
@@ -1,10 +1,21 @@
 #pragma once
 #include <string>
 
+struct QueryObject
+{
+	QueryObject() = delete;
+	QueryObject(const std::wstring_view queryString, bool isNeedResult)
+		: query(queryString), needResult(isNeedResult)
+	{}
+
+	const std::wstring query;
+	bool needResult;
+};
+
 struct AuthQuerySet
 {
 private: 
-	AuthQuerySet() = default;
+	AuthQuerySet();
 	~AuthQuerySet() = default;
 	AuthQuerySet(const AuthQuerySet& other) = delete;
 	AuthQuerySet& operator=(const AuthQuerySet& other) = delete;
@@ -26,6 +37,6 @@ public:
 	// output :
 	//			void
 	///////////////////////////////
-	//const std::wstring exampleQuery = "{CALL ExampleSPName(?, ?)}";
+	// const QueryObject exampleQueryObject;
 #pragma endregion Query
 };
