1c94b19174fe65f6a8c9648453d193b3fb120b62
m5623skhj <m5623skhj@gmail.com>
2024-03-03 20:08:34 +0900

    * 로그 타입이 Succes / Failed 이면 너무 범용적이어서, 앞에 Query를 추가

--- AuthServer/AuthServer/NeedDBResultHandler.cpp
+++ AuthServer/AuthServer/NeedDBResultHandler.cpp
@@ -14,12 +14,12 @@ void AuthDBThreadManager::NeedDBResultHandler(DBConnection& conn, const DBJobObj
 	case QueryType::InvalidQueryType:
 	default:
 		std::cout << "Invalid query type. Check query type" << std::endl;
-		_LOG(LOG_LEVEL::LOG_ERROR, L"Failed", L"Query : %ls / SessionId : %lld / ClientType : %d"
+		_LOG(LOG_LEVEL::LOG_ERROR, L"Query Fail", L"Query : %ls / SessionId : %lld / ClientType : %d"
 			, jobObject.queryObject, jobObject.sessionId, jobObject.clientType);
 
 		return;
 	}
 
-	_LOG(LOG_LEVEL::LOG_DEBUG, L"Success", L"Query : %ls / SessionId : %lld / ClientType : %d"
+	_LOG(LOG_LEVEL::LOG_DEBUG, L"Query Success", L"Query : %ls / SessionId : %lld / ClientType : %d"
 		, jobObject.queryObject, jobObject.sessionId, jobObject.clientType);
 }
