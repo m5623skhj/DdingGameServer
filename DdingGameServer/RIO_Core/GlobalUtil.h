3c39088ed839131ba0cb37ced9627f87bb59b2e9
m5623skhj <m5623skhj@gmail.com>
2024-08-03 00:21:52 +0900

    * 정수형 타입 캐스팅 시 정상적으로 캐스팅 되는지에 대한 글로벌 유틸 함수 추가

--- DdingGameServer/RIO_Core/GlobalUtil.h
+++ DdingGameServer/RIO_Core/GlobalUtil.h
@@ -2,3 +2,12 @@
 #include "EnumType.h"
 
 bool IsSuccess(const ERROR_CODE& errorCode);
+
+template<typename FromType, typename ToType>
+requires std::is_integral_v<FromType>
+and std::is_integral_v<ToType>
+bool GuardIntegralTypeCasting(FromType value, OUT ToType& result)
+{
+	result = static_cast<ToType>(value);
+	return result == value;
+}
